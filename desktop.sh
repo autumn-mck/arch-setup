e1733a808ab1a15836cfef7b78cf48c28e152afb
Autumn McKee <autumn@mck.is>
2024-04-19 21:33:22 +0100

    fix fiio k3 service setup

--- desktop.sh
+++ desktop.sh
@@ -2,9 +2,9 @@
 set -e
 
 # constant audio to fix issues when stopping/starting
-mkdir -p .config/systemd/user/
+mkdir -p "$HOME/.config/systemd/user/"
 cp "./config/fiio-k3-fix.service" "$HOME/.config/systemd/user/"
-.config/systemd/user/fiio-k3-fix.service
+systemctl --user enable --now fiio-k3-fix
 
 # syncthing - devices
 syncthing cli config devices add --device-id KKYBFUF-45PZX63-5INJFI2-YAWLHGI-D63E3SN-5REEN4C-5UZF2BL-WF5UWAC --name hazel
