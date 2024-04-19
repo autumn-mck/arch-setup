86fd3c41d21344f34b87555ea4c77ccd1d0a2918
Autumn McKee <autumn@mck.is>
2024-04-20 00:50:52 +0100

    amd.sh

---
+++ amd.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -e
+
+paru -S --needed --noconfirm mesa lib32-mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau amdgpu_top
