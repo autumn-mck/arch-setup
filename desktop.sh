53e666511952e670dc32624944ad1f798290eb06
Autumn McKee <autumn@mck.is>
2024-04-19 21:33:08 +0100

    remove librewolf native messaging bridge

--- desktop.sh
+++ desktop.sh
@@ -9,7 +9,3 @@ cp "./config/fiio-k3-fix.service" "$HOME/.config/systemd/user/"
 # syncthing - devices
 syncthing cli config devices add --device-id KKYBFUF-45PZX63-5INJFI2-YAWLHGI-D63E3SN-5REEN4C-5UZF2BL-WF5UWAC --name hazel
 syncthing cli config devices add --device-id WXUPQ65-XTVKWGV-FRKVLNV-YIS6CRY-BHS5MTL-UDCL3DR-W4QLMV5-35OQCQD --name spruce
-
-# librewolf pwas for firefox
-ln -s ~/.mozilla/native-messaging-hosts ~/.librewolf/native-messaging-hosts
-sudo ln -s /usr/lib/mozilla/native-messaging-hosts /usr/lib/librewolf/native-messaging-hosts
